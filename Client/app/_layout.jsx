f551aa4dabd4db850997976a1bdba0a834497e03
RivotX <miguelrosso60@gmail.com>
2024-09-04 21:51:30 +0200

    validaciones update user + ver foto perfil

--- Client/app/_layout.jsx
+++ Client/app/_layout.jsx
@@ -1,64 +1,68 @@
 //Client/app/_layout.jsx
-import { useEffect, useState } from 'react';
-import { Stack } from 'expo-router';
-import tw from 'twrnc';
-import { Image, View, Text, SafeAreaView, TouchableOpacity, Modal } from 'react-native';
-import ProfileIcon from '../assets/images/ProfileIcon.png';
-import MainLogin from './MainLogin';
-import ConfigIcon from '../components/ConfigIcon';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { useThemeColor } from '../hooks/useThemeColor';
-import UserProfileModal from '../components/modals/UserProfileModal';
-import getEnvVars from '../config';
-import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { SocketProvider } from '../components/context/SocketContext';
-import createSocket from '../components/context/CreateSocket';
-import { Audio } from 'expo-av';
+import { useEffect, useState } from "react";
+import { Stack } from "expo-router";
+import tw from "twrnc";
+import { Image, View, Text, SafeAreaView, TouchableOpacity, Modal } from "react-native";
+import ProfileIcon from "../assets/images/ProfileIcon.png";
+import MainLogin from "./MainLogin";
+import ConfigIcon from "../components/ConfigIcon";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { useThemeColor } from "../hooks/useThemeColor";
+import UserProfileModal from "../components/modals/UserProfileModal";
+import getEnvVars from "../config";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { SocketProvider } from "../components/context/SocketContext";
+import createSocket from "../components/context/CreateSocket";
+import { Audio } from "expo-av";
 
 export default function RootLayout() {
   const [modalIconVisible, setModalIconVisible] = useState(false);
-  const SoftbackgroundColor = useThemeColor({}, 'Softbackground');
-  const textColor = useThemeColor({}, 'text');
-  const [username, setUsername] = useState('username');
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const SoftbackgroundColor = useThemeColor({}, "Softbackground");
+  const textColor = useThemeColor({}, "text");
+  const [username, setUsername] = useState("username");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isSocketConnected, setIsSocketConnected] = useState(false);
 
   SetLayoutLogged = (value) => {
-    setIsLoggedIn(value)
+    setIsLoggedIn(value);
   };
 
   // Controla si la sesión esta logeada
   useEffect(() => {
-    checkLoginStatus()
+    checkLoginStatus();
   }, []);
 
   const checkLoginStatus = async () => {
-    const loggedIn = await AsyncStorage.getItem('isLoggedIn');
-    setIsLoggedIn(loggedIn === 'true');
+    const loggedIn = await AsyncStorage.getItem("isLoggedIn");
+    setIsLoggedIn(loggedIn === "true");
   };
 
   const { SERVER_URL } = getEnvVars();
   const { SOCKET_URL } = getEnvVars();
   useEffect(() => {
-    axios.get(`http://localhost:3000/getsession`, { withCredentials: true })
+    axios
+      .get(`http://localhost:3000/getsession`, { withCredentials: true })
       // axios.get(`${SERVER_URL}/getsession`, { withCredentials: true })
       .then((res) => {
-        setUsername(res.data.user.username)
+        setUsername(res.data.user.username);
       })
-      .catch((error) => { console.log(error) });
-  }, [isLoggedIn])
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [isLoggedIn]);
 
   // logout
   const handleLogout = async () => {
-    axios.post(`${SERVER_URL}/logout`)
+    axios
+      .post(`${SERVER_URL}/logout`)
       .then((res) => {
         if (socket) {
-          socket.disconnect();  // Desconectar el socket al desloguear
-          console.log('Socket desconectado en logout');
+          socket.disconnect(); // Desconectar el socket al desloguear
+          console.log("Socket desconectado en logout");
           setIsSocketConnected(false);
         }
-        AsyncStorage.removeItem('isLoggedIn');
+        AsyncStorage.removeItem("isLoggedIn");
         setIsLoggedIn(false);
       })
       .catch((error) => {
@@ -69,31 +73,29 @@ export default function RootLayout() {
 
   useEffect(() => {
     if (socket != null) {
-      socket.on('connect', () => {
+      socket.on("connect", () => {
         setIsSocketConnected(true);
-        console.log('ESTA CONECTADO');
+        console.log("ESTA CONECTADO");
       });
     }
   }, [socket]);
 
-useEffect(() => { // UseEffect para recibir los audios en cualquier parte de la app
+  useEffect(() => {
+    // UseEffect para recibir los audios en cualquier parte de la app
     if (socket != null) {
-      socket.on('receive-audio', async (base64Audio, room) => {
-        console.log('Received audio data from room', room);
+      socket.on("receive-audio", async (base64Audio, room) => {
+        console.log("Received audio data from room", room);
         const uri = `data:audio/wav;base64,${base64Audio}`;
         console.log("audio enviado", uri);
 
         // Play audio using expo-av
-        const { sound } = await Audio.Sound.createAsync(
-          { uri },
-          { shouldPlay: true }
-        );
+        const { sound } = await Audio.Sound.createAsync({ uri }, { shouldPlay: true });
         await sound.setVolumeAsync(1.0); // Ensure volume is set to maximum
         await sound.playAsync();
       });
 
       return () => {
-        socket.off('receive-audio');
+        socket.off("receive-audio");
       };
     }
   }, [socket]);
@@ -102,96 +104,94 @@ useEffect(() => { // UseEffect para recibir los audios en cualquier parte de la 
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaView style={{ flex: 1 }}>
         {/* Alvaro comenta la linea de abajo u.u */}
-        {isLoggedIn && isSocketConnected ? (
-          <SocketProvider socket={socket}>
-            <Stack screenOptions={{ animation: 'slide_from_right', }} >
-              <Stack.Screen
-                name="(tabs)"
-                options={{
-                  headerLeft: () => (
-                    <View style={tw`flex-row items-center `}>
-                      <Image source={ProfileIcon} style={tw`w-8 h-8 mr-2`} />
-                      <Text style={tw`text-base font-semibold text-[${textColor}]`}>{username} </Text>
-                    </View>
+        {/* {isLoggedIn && isSocketConnected ? ( */}
+        <SocketProvider socket={socket}>
+          <Stack screenOptions={{ animation: "slide_from_right" }}>
+            <Stack.Screen
+              name="(tabs)"
+              options={{
+                headerLeft: () => (
+                  <View style={tw`flex-row items-center `}>
+                    <Image source={ProfileIcon} style={tw`w-8 h-8 mr-2`} />
+                    <Text style={tw`text-base font-semibold text-[${textColor}]`}>{username} </Text>
+                  </View>
+                ),
+                headerRight: () => <ConfigIcon handleLogout={handleLogout} />,
+                headerTitle: "",
+                headerTitleAlign: "center",
+                headerStyle: tw`bg-[${SoftbackgroundColor}]`,
+              }}
+            />
+            <Stack.Screen
+              name="AddContactsScreen"
+              options={{
+                headerStyle: {
+                  backgroundColor: SoftbackgroundColor, // Dark background color for the header
+                },
+                headerTintColor: textColor,
+                headerTitle: "Add Contacts",
+              }}
+            />
+            <Stack.Screen
+              name="AddGroupsScreen"
+              options={{
+                headerStyle: {
+                  backgroundColor: SoftbackgroundColor, // Dark background color for the header
+                },
+                headerTintColor: textColor,
+                headerTitle: "Add Groups",
+              }}
+            />
+            <Stack.Screen
+              name="ChatRoom"
+              options={({ route }) => {
+                const user = route.params.user; // Correctly access the user object from route params
+                return {
+                  headerStyle: {
+                    backgroundColor: SoftbackgroundColor,
+                  },
+                  headerTintColor: textColor,
+                  headerTitle: () => (
+                    <TouchableOpacity onPress={() => setModalIconVisible(true)} style={tw`w-full`}>
+                      <View style={tw`flex-1 flex-row justify-start items-center w-full`}>
+                        <UserProfileModal user={user} modalIconVisible={modalIconVisible} setModalIconVisible={setModalIconVisible} iconSize={12} />
+                        <Text style={tw`text-[${textColor}] font-bold text-lg ml-3`}>{user.name || "Chat Room"}</Text>
+                      </View>
+                    </TouchableOpacity>
                   ),
-                  headerRight: () => (
-                    <ConfigIcon handleLogout={handleLogout} />
-                  ),
-                  headerTitle: '',
-                  headerTitleAlign: 'center',
-                  headerStyle: tw`bg-[${SoftbackgroundColor}]`,
-                }}
-              />
-              <Stack.Screen
-                name="AddContactsScreen"
-                options={{
-                  headerStyle: {
-                    backgroundColor: SoftbackgroundColor, // Dark background color for the header
-                  },
-                  headerTintColor: textColor,
-                  headerTitle: 'Add Contacts',
-                }}
-              />
-              <Stack.Screen
-                name="AddGroupsScreen"
-                options={{
-                  headerStyle: {
-                    backgroundColor: SoftbackgroundColor, // Dark background color for the header
-                  },
-                  headerTintColor: textColor,
-                  headerTitle: 'Add Groups',
-                }}
-              />
-              <Stack.Screen
-                name="ChatRoom"
-                options={({ route }) => {
-                  const user = route.params.user; // Correctly access the user object from route params
-                  return {
-                    headerStyle: {
-                      backgroundColor: SoftbackgroundColor,
-                    },
-                    headerTintColor: textColor,
-                    headerTitle: () => (
-                      <TouchableOpacity onPress={() => setModalIconVisible(true)} style={tw`w-full`}>
-                        <View style={tw`flex-1 flex-row justify-start items-center w-full`}>
-                          <UserProfileModal
-                            user={user}
-                            modalIconVisible={modalIconVisible}
-                            setModalIconVisible={setModalIconVisible}
-                            iconSize={12}
-                          />
-                          <Text style={tw`text-[${textColor}] font-bold text-lg ml-3`}>
-                            {user.name || 'Chat Room'}
-                          </Text>
-                        </View>
-                      </TouchableOpacity>
-                    ),
-                    headerLeft: () => (
-                      <View style={{ marginLeft: -50 }} />
-                    ),
-                  };
-                }}
-              />
-              <Stack.Screen
-                name="ProfileSettings"
-                options={{
-                  headerStyle: {
-                    backgroundColor: SoftbackgroundColor, // Dark background color for the header
-                  },
-                  headerTintColor: textColor,
-                  headerTitle: 'Settings',
-                }}
-              />
-            </Stack >
-          </SocketProvider>
+                  headerLeft: () => <View style={{ marginLeft: -50 }} />,
+                };
+              }}
+            />
+            <Stack.Screen
+              name="ProfileSettings"
+              options={{
+                headerStyle: {
+                  backgroundColor: SoftbackgroundColor,
+                },
+                headerTintColor: textColor,
+                headerTitle: "Settings",
+              }}
+            />
+            <Stack.Screen
+              name="ProfilePhoto"
+              options={{
+                headerStyle: {
+                  backgroundColor: SoftbackgroundColor,
+                },
+                headerTintColor: textColor,
+                headerTitle: "Profile Photo",
+              }}
+            />
+          </Stack>
+        </SocketProvider>
 
-        ) : (
-          //If not logged in, show the LoginScreen without navigation
-          <MainLogin SetLayoutLogged={SetLayoutLogged} />
-        )}
+        {/* ) : ( */}
+        {/* //If not logged in, show the LoginScreen without navigation */}
+        {/* <MainLogin SetLayoutLogged={SetLayoutLogged} /> */}
+        {/* )} */}
 
         {/* Alvaro comentam PRIMERO arriba del todo, después desde el ' ) ' hasta aqui  */}
-
       </SafeAreaView>
     </GestureHandlerRootView>
   );
--- Client/components/modals/ChangeProfileModal.jsx
+++ Client/components/modals/ChangeProfileModal.jsx
@@ -1,5 +1,5 @@
-import { React, useState, useEffect } from "react";
-import { Modal, View, Text, TextInput, TouchableOpacity } from "react-native";
+import { React, useState, useEffect, Platform } from "react";
+import { Modal, View, Text, TextInput, TouchableOpacity, Alert } from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import tw from "twrnc";
 import { useThemeColor } from "../../hooks/useThemeColor";
@@ -15,24 +15,66 @@ const ChangeProfileModal = ({ PropToChange, setModalVisibility, ModalIcon, isPas
       setNewProp(currentProp);
     }
   }, [isPassword, currentProp]);
+
+  // Function to show alert based on platform
+  const showAlert = (title, message) => {
+    if (Platform.OS === "web") {
+      alert(`${title}: ${message}`);
+    } else {
+      Alert.alert(title, message, [{ text: "OK" }]);
+    }
+  };
+
+  //validate form
+  const validateForm = () => {
+    if (newProp.trim().length === 0) {
+      showAlert("Invalid " + PropToChange, "The new " + PropToChange + " cannot be empty");
+      return false;
+    }
+    if (isPassword && newProp.trim().length < 8) {
+      showAlert("Invalid password", "Password must be at least 8 characters long.");
+      return false;
+    }
+    if (PropToChange === "email") {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(newProp)) {
+        showAlert("Invalid Email", "Please enter a valid email address.");
+        return false;
+      }
+    }
+    if (PropToChange === "info" && newProp.length > 120) {
+      showAlert("Invalid info", "Info must be less than 120 characters.");
+      return false;
+    }
+    setValidForm(true);
+    return true;
+  };
+
   // Update the user
   const updateUser = () => {
-    console.log('propToChange', PropToChange)
-    console.log('newProp', newProp)
-    axios.post('http://localhost:3000/update-user', {
-      userID: userID,
-      PropToChange: PropToChange,
-      newProp: newProp,
-    }).then((res) => {
-      refreshSession();
-    }).catch((err) => {
-      console.error('Error updating user:', err);
-      if (err.response && err.response.data) {
-        // Handle error (e.g., show an error message)
-      } else {
-        // Handle error (e.g., show an error message)
-      }
-    });
+    if (!validateForm()) return;
+
+    //if the form is valid, update the user
+    axios
+      .post("http://localhost:3000/update-user", {
+        userID: userID,
+        PropToChange: PropToChange,
+        newProp: newProp,
+      })
+      .then(() => {
+        refreshSession();
+      })
+      .then(() => {
+        setModalVisibility(false);
+      })
+      .catch((err) => {
+        console.error("Error updating user:", err);
+        if (err.response && err.response.data) {
+          // Handle error (e.g., show an error message)
+        } else {
+          // Handle error (e.g., show an error message)
+        }
+      });
   };
 
   return (
@@ -42,16 +84,21 @@ const ChangeProfileModal = ({ PropToChange, setModalVisibility, ModalIcon, isPas
           <Text style={tw`text-lg font-bold mb-4 text-[${textColor}]`}>Change your {PropToChange}</Text>
           <View style={tw`flex-row items-center border border-gray-300 rounded p-2`}>
             <Ionicons name={ModalIcon} size={20} color="gray" style={tw`mr-2`} />
-            <TextInput placeholder={`Enter new ${PropToChange}`} value={newProp}
-
-              secureTextEntry={isPassword} style={tw`flex-1 text-[${textColor}]`} onChange={(e) => { setNewProp(e.target.value) }} />
+            <TextInput
+              placeholder={`Enter new ${PropToChange}`}
+              value={newProp}
+              secureTextEntry={isPassword}
+              style={tw`flex-1 text-[${textColor}]`}
+              onChange={(e) => {
+                setNewProp(e.target.value);
+              }}
+            />
           </View>
           <TouchableOpacity
             style={tw`mt-4 bg-blue-500 rounded p-2 items-center`}
             onPress={() => {
               // Handle password change logic here
-              setModalVisibility(false);
-              updateUser()
+              updateUser();
             }}>
             <Text style={tw`text-white`}>Change {PropToChange}</Text>
           </TouchableOpacity>
